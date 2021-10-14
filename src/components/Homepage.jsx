716cddd97ffa5524906faacad6e2e3df4fda280e
Mario Rizki <mariorizki07@gmail.com>
2021-10-14 16:07:44 +0700

    display global crypto stats

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,7 +9,9 @@ const { Title } = Typography;
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery();
-  console.log(data);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return 'Loading';
 
   return (
     <>
@@ -18,19 +20,31 @@ const Homepage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={5} />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={5} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value={5} />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h volume" value={5} />
+          <Statistic
+            title="Total 24h volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={5} />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
     </>
