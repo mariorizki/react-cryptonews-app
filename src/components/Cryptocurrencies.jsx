0541f2e076a1570fd18b5ab10ddf87385eb88e78
Mario Rizki <mariorizki07@gmail.com>
2021-10-14 17:28:54 +0700

    display all cryptos

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -10,7 +10,27 @@ const Cryptocurrencies = () => {
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
   console.log(cryptos);
-  return <div>Cryptocurrencies</div>;
+  return (
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos.map(currency => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Changes: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Cryptocurrencies;
