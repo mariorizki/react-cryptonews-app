6f3033df5449f98109e95cf1105d375171b02a76
Mario Rizki <mariorizki07@gmail.com>
2021-10-14 17:14:38 +0700

    test fetch all cryptos from rapid api

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
@@ -29,10 +30,8 @@ const Homepage = () => {
           />
         </Col>
         <Col span={12}>
-          <Statistic
-            title="Total Market Cap"
-            value={millify(globalStats.totalMarketCap)}
-          />
+          title="Total Market Cap"
+          <Statistic value={millify(globalStats.totalMarketCap)} />
         </Col>
         <Col span={12}>
           <Statistic
@@ -47,6 +46,22 @@ const Homepage = () => {
           />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <Title level={2} className="home-title">
+        Latest Crypto News
+      </Title>
+      <Title level={3} className="show-more">
+        <Link to="/news">Show More</Link>
+      </Title>
+      <News simplified />
     </>
   );
 };
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -7,15 +7,6 @@ const cryptoApiHeaders = {
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/stats',
-//     headers: {
-//         'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//         'x-rapidapi-key': '8fe0b369bemsh2e19d64801e26afp105b41jsncce8a01ce5f5'
-//     }
-// };
-
 const createRequest = url => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
