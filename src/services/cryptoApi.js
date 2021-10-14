6b74e596754783e8c8046675961ba1a252a67dec
Mario Rizki <mariorizki07@gmail.com>
2021-10-14 15:47:26 +0700

    setup base redux toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': '8fe0b369bemsh2e19d64801e26afp105b41jsncce8a01ce5f5'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/stats';
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//         'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//         'x-rapidapi-key': '8fe0b369bemsh2e19d64801e26afp105b41jsncce8a01ce5f5'
+//     }
+// };
+
+const createRequest = url => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: builder => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins')
+    })
+  })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
