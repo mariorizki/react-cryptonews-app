c4876733c83bc2e51c5bc23a790802249ec8c10f
Mario Rizki <mariorizki07@gmail.com>
2021-10-15 14:13:00 +0700

    display news with minimalist style

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -12,10 +12,26 @@ const News = ({ simplified }) => {
     newsCategory: 'Cryptocurrency',
     count: simplified ? 6 : 12
   });
-
+  if (!cryptoNews?.value) return 'Loading...';
   console.log(cryptoNews);
 
-  return <div>News</div>;
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={6} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-Title" level={4}>
+                  {news.name}
+                </Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
