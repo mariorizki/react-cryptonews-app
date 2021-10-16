dfc3703892f3b3bfec81df7cee23006195fa7252
Mario Rizki <mariorizki07@gmail.com>
2021-10-16 19:16:59 +0700

    done with exchanges page

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -22,7 +22,10 @@ export const cryptoApi = createApi({
     getCryptoHistory: builder.query({
       query: ({coinId, timePeriod}) => createRequest(`/coin/${coinId}/history/${timePeriod}`)
     }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
   })
 });
 
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery } = cryptoApi;
